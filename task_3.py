ec9af49829f0c53d44b8ef6c43ed62d8aee1502d
b0dun1dZe <108175458+b0dun1dZe@users.noreply.github.com>
2022-06-25 17:01:43 +0500

    Add files via upload

---
+++ task_3.py
@@ -0,0 +1,39 @@
+print('Задача 3. Число наоборот 2')
+
+# Пользователь вводит два числа — N и K.
+# Напишите программу,
+# которая заменяет каждое число на число,
+# которое получается из исходного записью его цифр в обратном порядке,
+# затем складывает их,
+# снова переворачивает и выводит ответ на экран.
+
+# Пример:
+
+# Введите первое число: 102
+# Введите второе число: 123
+
+# Первое число наоборот: 201
+# Второе число наоборот: 321
+
+# Сумма: 522
+# Сумма наоборот: 225
+
+
+def reverse(number):
+    reverseNumber = ''
+    while number != 0:
+        reverseNumber += str(number % 10)
+        number //= 10
+    return reverseNumber
+
+
+userFirstNumber = int(input('Введите первое число: '))
+userSecondNumber = int(input('Введите второе число: '))
+
+print(f'\nПервое число наоборот: {reverse(userFirstNumber)}')
+print(f'Второе число наоборот: {reverse(userSecondNumber)}')
+
+print(
+    f'\nСумма: {int(reverse(userFirstNumber)) + int(reverse(userSecondNumber))}'
+)
+print(f'Сумма наоборот: {userFirstNumber + userSecondNumber}')
---
+++ task_8.py
@@ -0,0 +1,45 @@
+print('Задача 8. Сумма ряда')
+
+# Пользователь вводит действительное число
+# "х" и точность "precision".
+
+# P.S: Формулу смотреть на сайте :)
+
+
+# Напишите программу,
+# которая по число х вычисляет сумму ряда в точности до precision.
+
+
+# Операцией возведения в степень и функцией factorial пользоваться нельзя.
+
+# Пример:
+# Введите точность: 0.001
+
+# Введите x: 5
+# Сумма ряда =  0.2836250150891709
+
+
+userNumber = float(input('Введите x: '))
+userPrecision = float(input('Введите точность: '))
+
+def factorial(a):
+  b = 1
+  for i in range(1, a + 1):
+    b *= i
+  return b
+
+def degree(a, n):
+  b = 1
+  for i in range(n):
+    b *= a
+  return b
+
+def total(x, n):
+  return degree(-1, n) * (degree(x, 2 * n) / factorial(2 * n))
+
+result = 0
+currentStep = 0
+while abs(total(userNumber, currentStep)) >= userPrecision:
+  result += total(userNumber, currentStep)
+  currentStep += 1
+print(f'Сумма ряда = {result}')
