d4516193d88c2c1a253c2a33bc19765c52fdd196
b0dun1dZe <b0dun1dze@yandex.ru>
2022-06-25 17:34:12 +0500

    'Description'

--- task_8.py
+++ task_8.py
@@ -22,24 +22,28 @@ print('Задача 8. Сумма ряда')
 userNumber = float(input('Введите x: '))
 userPrecision = float(input('Введите точность: '))
 
-def factorial(a): #Нахождение факториала
-  b = 1
-  for i in range(1, a + 1):
-    b *= i
-  return b
 
-def degree(a, n): #Возведение в степень
-  b = 1
-  for i in range(n):
-    b *= a
-  return b
+def factorial(a):  # Нахождение факториала
+    b = 1
+    for i in range(1, a + 1):
+        b *= i
+    return b
 
-def total(x, n):
-  return degree(-1, n) * (degree(x, 2 * n) / factorial(2 * n))
+
+def degree(a, n):  # Возведение в степень
+    b = 1
+    for i in range(n):
+        b *= a
+    return b
+
+
+def total(x, n):  # Формула нахождения суммы ряда
+    return degree(-1, n) * (degree(x, 2 * n) / factorial(2 * n))
+
 
 result = 0
 currentStep = 0
 while abs(total(userNumber, currentStep)) >= userPrecision:
-  result += total(userNumber, currentStep)
-  currentStep += 1
+    result += total(userNumber, currentStep)
+    currentStep += 1
 print(f'Сумма ряда = {result}')
